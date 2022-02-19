6b70bc0d7b94844cd187777bf20d2734dad92c5e
Chulhyun Jeon <slowstarter80@daum.net>
2022-02-19 23:29:35 +0900

    implemented simple calculator example

---
+++ calculator/calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "mymath.h"
+#include "mylog.h"
+
+int main(void)
+{
+	int input1 = 0, input2 = 0;
+	int result_add, result_sub, result_mul, result_div, result_rem;
+
+	printf("This is a calculator application!\n");
+	printf("Input two numbers\n");
+
+	scanf("%d%d", &input1, &input2);
+
+	result_add = addition(input1, input2);
+	result_sub = subtraction(input1, input2);
+	result_mul = multiplication(input1, input2);
+	result_div = division(input1, input2);
+	result_rem = remainder(input1, input2);
+
+	msg("result_add = %d\n", result_add);
+	msg("result_sub = %d\n", result_sub);
+	msg("result_mul = %d\n", result_mul);
+	msg("result_div = %d\n", result_div);
+	msg("result_rem = %d\n", result_rem);
+
+	return 0;
+}
