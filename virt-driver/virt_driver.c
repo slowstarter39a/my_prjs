8ddd06f01479627ae11427e1ae7caf379514ba24
Chulhyun Jeon <slowstarter80@daum.net>
2022-02-26 22:02:51 +0900

    Compile error fix

--- virt-driver/virt_driver.c
+++ virt-driver/virt_driver.c
@@ -1,22 +1,20 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init_module(void)
+int init_virt_driver_module(void)
 {
-	pr_info("%s\n");
+	pr_info("%s\n", __func__);
 
 	return 0;
 }
 
-void exit_module(void)
+void exit_virt_driver_module(void)
 {
-	pr_info("%s\n");
+	pr_info("%s\n", __func__);
 }
 
-module_init(init_module);
-module_exit(exit_module);
-
-MODULE
+module_init(init_virt_driver_module);
+module_exit(exit_virt_driver_module);
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("Chulhyun");
